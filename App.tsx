58a2631a2aeba5a90e73133bfa32c94cb7ded7ad
Theodor Johansson <t.j.u@telia.com>
2023-11-12 17:44:48 +0100

    #1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './src/components/button'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Edited Open up App.tsx to start working on your app!</Text>
+      <Button />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View} from 'react-native'
+
+const Button = () => {
+    return (
+        <View>
+            <Text>Button</Text>
+        </View>
+    )
+}
+
+export default Button
+const styles = StyleSheet.create({})
